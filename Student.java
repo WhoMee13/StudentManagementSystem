d207097a3d886cba747c6405fc65a2b080067938
Nishith <nishithshrestha2005@gmail.com>
2025-01-13 10:22:32 +0545

    adding getter and setter in student.java file

--- Student.java
+++ Student.java
@@ -1,8 +1,20 @@
 public class Student {
-    String name;
-    int age;
+    private String name;
+    private int age;
     public Student(String name, int age){
         this.name=name;
         this.age=age;
+    }
+    void setName(String name){
+        this.name=name;
+    }
+    void setAge(int age){
+        this.age=age;
+    }
+    String getName(){
+        return this.name;
+    }
+    int getAge(){
+        return this.age;
     }
 }
